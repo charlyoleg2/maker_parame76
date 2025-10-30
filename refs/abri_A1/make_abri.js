9c45db113bc528e26a973d8f13e28545275debde
charlyoleg <charles.braquet@gmx.fr>
2025-10-30 11:51:29 +0100

    abri_A1

--- refs/abri_A1/make_abri.js
+++ refs/abri_A1/make_abri.js
@@ -23,12 +23,77 @@ async function execCmd(cmd) {
 	}
 }
 
+const opt1 = '-d=desi76/abri -p=px_abri_A1_v01.json';
+const opt2 = 'write scad_3d_openscad';
+const refName = 'abri_A1';
+
 //rCmd.push(`npx desi76-cli -d=desi76/${desiName} -p=refs/${dName}/px_${fName}.json -o=refs/${dName} --outFileName=${fName}.scad write scad_3d_openscad`);
 //rCmd.push(`openscad -o refs/${dName}/${fName}_oscad.stl refs/${dName}/${fName}.scad`);
+console.log('make_abri.js says Hello!');
 await execCmd('ls -la');
-await execCmd(`npx desi76-cli -d=desi76/abri -p=px_abri_A1_v01.json -m d3Plank1 1 -m d3Plank1West 0 -m d3Plank1East 0 -m d3Plank1SN 1 --outFileName=abri_A1_pl1_lolop1.scad write scad_3d_openscad`);
-await execCmd(`openscad -o output/abri_A1_pl1_lolop1_oscad.stl output/abri_A1_pl1_lolop1.scad`);
-await execCmd(`npx desi76-cli -d=desi76/abri -p=px_abri_A1_v01.json -m d3Plank5 1 --outFileName=abri_A1_pl5.scad write scad_3d_openscad`);
-await execCmd(`openscad -o output/abri_A1_pl5_oscad.stl output/abri_A1_pl5.scad`);
+// pl1
+let scadName = `${refName}_pl1_edlop1_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank1 1 -m d3Plank1West 2 -m d3Plank1East 0 -m d3Plank1SN 0 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl1_lolop1_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank1 1 -m d3Plank1West 0 -m d3Plank1East 0 -m d3Plank1SN 0 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl1_loedp1_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank1 1 -m d3Plank1West 0 -m d3Plank1East 2 -m d3Plank1SN 0 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl1_edlop4_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank1 1 -m d3Plank1West 2 -m d3Plank1East 0 -m d3Plank1SN 3 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl1_lolop4_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank1 1 -m d3Plank1West 0 -m d3Plank1East 0 -m d3Plank1SN 3 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl1_loedp4_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank1 1 -m d3Plank1West 0 -m d3Plank1East 2 -m d3Plank1SN 3 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pl2
+scadName = `${refName}_pl2EE_x4`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank2EE 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pl3
+scadName = `${refName}_pl3EE_x6`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank3EE 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pl4
+scadName = `${refName}_pl4S_x3`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank4S 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl4N_x3`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank4N 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pl5
+scadName = `${refName}_pl5_x3`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank5 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pl6
+scadName = `${refName}_pl6_x1`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank6 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pl7
+// pl8
+scadName = `${refName}_pl8S_x3`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank8S 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+scadName = `${refName}_pl8N_x3`;
+await execCmd(`npx desi76-cli ${opt1} -m d3Plank8N 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// plda
+scadName = `${refName}_plda_x6`;
+await execCmd(`npx desi76-cli ${opt1} -m d3PlankDiagA 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pldb
+scadName = `${refName}_pldb_x8`;
+await execCmd(`npx desi76-cli ${opt1} -m d3PlankDiagB 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// pldt
+scadName = `${refName}_pldt_x4`;
+await execCmd(`npx desi76-cli ${opt1} -m d3PlankDiagTop 1 --outFileName=${scadName}.scad ${opt2}`);
+await execCmd(`openscad -o output/${scadName}_oscad.stl output/${scadName}.scad`);
+// finish
 await execCmd('ls -la output');
+console.log('make_abri.js says Bye!');
 
